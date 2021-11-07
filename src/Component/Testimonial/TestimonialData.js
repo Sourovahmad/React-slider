19ea3dfc97fe0cbf539d5d3a19d49fba0c00a6f0
sourovahmad <mdsourovahmad19@gmail.com>
2021-11-07 21:00:40 +0600

    slider image

---
+++ src/Component/Testimonial/TestimonialData.js
@@ -0,0 +1,10 @@
+
+import MyImage from '../../images/slider_image/Customer-testimonial-page.jpg';
+
+export const TestimonialData = [ 
+    {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+  
+];
--- src/Component/Testimonial/TestimonialSlider/TestimonialSlider.jsx
+++ src/Component/Testimonial/TestimonialSlider/TestimonialSlider.jsx
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import  {TestimonialData} from "../TestimonialData";
 import "./TestimonialSlider.css";
 
+
 const TestimonialSlider = () => {
+
+
+    useEffect(() => {
+
+        const FirstItem = TestimonialData.find(data => data.id === 1);
+        setcurrentContent(FirstItem);
+       
+    }, []);
+
+    const [currentContent, setcurrentContent] = useState([]);
+
+
   return (
     <>
       <div className="testimonial_box">
 
-
           <div className="prev_button">
-                  <span><i class="fas fa-arrow-left"></i></span>
+            <span>
+              <i class="fas fa-arrow-left"></i>
+            </span>
           </div>
-
-
-
 
           <div className="nex_button">
-                    <span> <i class="fas fa-arrow-right"></i>  </span>
+            <span>
+              <i class="fas fa-arrow-right"></i>
+            </span>
           </div>
+
+          <div className="slider_content_section">
+              <div className="slider_text_Section m-4 p-3">
+                  <p>
+                    {
+                        currentContent.text
+                    }
+                  </p>
+              </div>
+
+              <div className="slider_client_title">
+                  <h4>
+                      JHON DEO
+                  </h4>
+
+                <h5> UI/UX Desginer</h5>
+              </div>
+
+
+              <div className="slider_image_section">
+              <img src={currentContent.image} alt="" />
+              </div>
+          </div>
+
 
       </div>
     </>
