58a4274a43fc90392d50d5c10d791731b5c8cde0
sourovahmad <mdsourovahmad19@gmail.com>
2021-11-07 21:05:28 +0600

    update the animation =

--- src/Component/Testimonial/TestimonialData.js
+++ src/Component/Testimonial/TestimonialData.js
@@ -3,6 +3,47 @@ import MyImage from '../../images/slider_image/Customer-testimonial-page.jpg';
 
 export const TestimonialData = [ 
     {
+        id: 1,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+    {
+        id: 2,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+    {
+        id: 3,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+    {
+        id: 4,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+    {
+        id: 5,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+    {
+        id: 6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+    {
+        id: 7,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+    {
+        id: 8,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
+        image: MyImage,
+    },
+    {
+        id: 9,
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore, nam veniam tenetur quod laboriosam maiores expedita at tempora eos, adipisci ad mollitia rerum qui dignissimos commodi, corrupti perspiciatis. Eos!",
         image: MyImage,
     },
